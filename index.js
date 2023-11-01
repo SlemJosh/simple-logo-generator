3ee8d8489e1a433e5474539ee11784314d130b1b
SlemJosh <joshua.slem@gmail.com>
2023-11-01 15:12:20 -0600

    Added the questions and answers for the prompts

--- index.js
+++ index.js
@@ -7,5 +7,37 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // We will also need to use our created functions
-// Coming soon
+const { Circle, Square, Triangle } = require(".lib/shapes");
 
+// Prompts for User
+
+function logoQuestions(){
+    inquirer
+    .prompt([
+        // Text
+        {
+            type: "input",
+            message: "What text do you want in your logo? (Enter up to three characters)",
+            name: "text",
+        },
+        // Color of Text
+        {
+            type: "input",
+            message: "What color would you like your text to be? (Enter color OR hexadecimal value)",
+            name: "textColor",
+        },
+        // Shape
+        {
+            type: "list",
+            message: "What shape would you like to use for your logo?",
+            choices: ["Circle", "Square", "Triangle"],
+            name: "shape",
+        },
+        // Shape Color
+        {
+            type: "input",
+            message: "What color would you like your shape to be? (Enter color OR hexaddecimal value)",
+        },
+    ])
+    
+}
