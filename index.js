909eea683e0ac6985b7d20a715120b0a827d4269
SlemJosh <joshua.slem@gmail.com>
2023-11-01 14:30:51 -0600

    Started by adding the npms to our index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// Import the npms we need
+
+// inquirer for adding prompts to the user to gather needed information
+const inquirer = require("inquirer");
+
+// fs for creating files
+const fs = require("fs");
+
+// We will also need to use our created functions
+// Coming soon
+
